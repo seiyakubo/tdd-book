c029570b81b1d3f2c2cde2f8a2edbf6e68caaebe
Seiya KUBO <s.kubo@i3design.co.jp>
2018-11-12 22:55:12 +0900

    end of Chapter 8

--- src/Money.php
+++ src/Money.php
@@ -2,12 +2,24 @@
 declare(strict_types=1);
 namespace I3D;
 
-class Money
+abstract class Money
 {
     protected $amount;
+
+    abstract public function times(int $multiplier): Money;
 
     public function equals(Money $money): bool
     {
         return $this->amount === $money->amount && get_class($this) === get_class($money);
+    }
+
+    public static function dollar(int $amount): Money
+    {
+        return new Dollar($amount);
+    }
+
+    public static function franc(int $amount): Money
+    {
+        return new Franc($amount);
     }
 }
